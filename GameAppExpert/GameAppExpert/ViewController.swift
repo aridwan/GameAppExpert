437e8604a50d285598a01371d63c60087d3fe2fd
M. Arief Ridwan <aridwan05@gmail.com>
2023-03-13 13:17:33 +0700

    project

---
+++ GameAppExpert/GameAppExpert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameAppExpert
+//
+//  Created by Mochammad Arief Ridwan on 13/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
